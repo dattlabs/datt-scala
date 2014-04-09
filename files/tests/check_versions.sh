5a698b5a85f01e0a8146a0e8f367cc06697562a9
Jonathan Leonard <johanatan@gmail.com>
2014-04-09 15:54:03 -0700

    Check sbt version.

--- files/tests/check_versions.sh
+++ files/tests/check_versions.sh
@@ -7,4 +7,7 @@ scalac_version=`scalac -version 2>&1 | grep "^Scala compiler version" | grep -oP
 scala_version=`scala -version 2>&1 | grep "^Scala code runner" | grep -oP '\K(\d+\.\d+\.\d+)'`
 [[ "$scala_version" = "2.10.3" ]] || { echo "Incorrect scala version: $scala_version."; status=1; }
 
+sbt_version=`sbt --version 2>&1 | grep version | grep -oP '\d+\.\d+\.\d+'`
+[[ "$sbt_version" = "0.13.1" ]] || { echo "Incorrect sbt version: $sbt_version."; status=1; }
+
 exit $status
